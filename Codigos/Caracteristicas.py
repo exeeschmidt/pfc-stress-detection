e44ab36534403ae2686333b843f2a786286b6b36
Hernan <tonu17695@gmail.com>
2020-04-07 15:27:19 -0300

    Cambios 07/04/20
    Weka.py:
     -No se ejecuta más JVM en estas clases
     -Vuelta de la división de los métodos de selección de características, clasificación y carga y filtrado. 
    
    main.py
     -Ejecuta JVM. Evita el error fatal entre la compilación de weka y las librerías provenientes de MATLAB

--- Codigos/Caracteristicas.py
+++ Codigos/Caracteristicas.py
@@ -83,6 +83,8 @@ class VideoEntero:
         if self.bool_metodos[2]:
             # Cargo el archivo con las caracteristicas hog
             hog, inds_hog = hrm.leeHOG('Procesado' + os.sep + nombre + '.hog')
+        else:
+            hog = np.array([])
 
         AUs = np.array([])
         if self.bool_metodos[3]:
@@ -244,7 +246,6 @@ class VideoEnParte:
                 print("Ruta de archivo incorrecta o no valida")
                 return
             video = cv.VideoCapture(path)
-            frames_totales = int(video.get(cv.CAP_PROP_FRAME_COUNT))
 
             archivo = hrm.leeCSV('Procesado' + os.sep + nombre + '.csv')
 
@@ -257,6 +258,8 @@ class VideoEnParte:
             if self.bool_metodos[2]:
                 # Cargo el archivo con las caracteristicas hog
                 hog, inds_hog = hrm.leeHOG('Procesado' + os.sep + nombre + '.hog')
+            else:
+                hog = np.array([])
 
             AUs = np.array([])
             if self.bool_metodos[3]:
